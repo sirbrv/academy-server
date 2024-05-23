bff9459919fe921c01d66b3570d4c46bb7d99c5f
Victor Brown <sirbrv@gmail.com>
2024-05-22 22:58:55 -0300

    Se elimina bcrypt

--- src/config/configMongoDB.js
+++ src/config/configMongoDB.js
@@ -6,7 +6,8 @@ require("dotenv").config({
 
 const dbmonto_host = process.env.DB_MONGO_HOST;
 const dbmongo_db = process.env.DB_DATABASE;
-const MONGODB_URL = `mongodb://${dbmonto_host}/${dbmongo_db}`;
+// const MONGODB_URL = `mongodb://${dbmonto_host}/${dbmongo_db}`;
+const MONGODB_URL = process.env.DB_MONGO_URI;
 
 mongoose
   .connect(MONGODB_URL, {})
