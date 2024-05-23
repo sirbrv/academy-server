4017953b561bad7ab2fc5641ccae8b82ba60bd9a
Victor Brown <sirbrv@gmail.com>
2024-05-22 21:08:04 -0300

    Se modifica configMongoDB

--- src/config/configMongoDB.js
+++ src/config/configMongoDB.js
@@ -3,9 +3,9 @@ const path = require("path");
 require("dotenv").config({
   path: path.join(__dirname, "../envirome/devEnvirome.env"),
 });
+
 const dbmonto_host = process.env.DB_MONGO_HOST;
 const dbmongo_db = process.env.DB_DATABASE;
-mongodb://u8empum37mchnqfxr2ht:y8npPKqnHdvPE25kd30@bjhy9zia87hifh9f6bfc-mongodb.services.clever-cloud.com:2109/bjhy9zia87hifh9f6bfc
 const MONGODB_URL = `mongodb://${dbmonto_host}/${dbmongo_db}`;
 
 mongoose
