5f10601260a1f858215432fd0545095075c776c9
Victor Brown <sirbrv@gmail.com>
2024-05-23 21:04:44 -0300

    Se elimina bcrypt

--- src/server.js
+++ src/server.js
@@ -16,13 +16,14 @@ require("./config/configMongoDB.js");
 /* ******************************************** */
 /*   **** Sección de Middleware                 */
 /* ******************************************** */
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    origin: "https://incredible-brioche-9fa76b.netlify.app",
+    credentials: true,
+  })
+);
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
